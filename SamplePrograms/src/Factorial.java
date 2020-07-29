2f7f1218df0adcc7479d1a813779fd18e73b6063
sumanth kumar reddy <sumanth kumar reddy@DESKTOP-GPGM25E>
2020-07-29 13:12:37 +0530

    Factorial Fibinocci series Sorting an Array

---
+++ SamplePrograms/src/Factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the number");
+		int c = s.nextInt();
+		int result = fact(c);
+		System.out.println("factorial of the number is "+result);
+
+	} 
+
+	public static int fact(int n) {
+		int i,fact=1;
+		for(i=1;i<=n;i++) {
+			fact=fact*i;
+
+		}
+
+		return fact;
+	}
+
+}
